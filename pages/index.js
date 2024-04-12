f967e6a6622cc39dffe084cf00fe4e29168f7e9b
subhro1530 <shaswata.ssaha@gmail.com>
2024-04-13 02:49:17 +0530

    z

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,11 @@ import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 // import Chatbot from "@/components/Chatbot";
 import dynamic from "next/dynamic"; // Import dynamic from next/dynamic for dynamic import
 
-// const Navbar = dynamic(() => import("../components/Navbar"));
-// const HeroSection = dynamic(() => import("../components/HeroSection"));
-// const Chatbot = dynamic(() => import("../components/Chatbot"));
+const Navbar = dynamic(() => import("../components/Navbar"),{ssr:false,});
+const HeroSection = dynamic(() => import("../components/HeroSection"), {
+  ssr: false,
+});
+const Chatbot = dynamic(() => import("../components/Chatbot"), { ssr: false });
 
 const Home = () => {
   return (
